689e4afc7ef1886b3390b28ed06d4fa33054b8fb
Nikhil Raju <nikhilraju.1511@gmail.com>
2021-06-22 14:18:34 -0400

    drop rows in place based on condition

--- pandas_useful_snippets.py
+++ pandas_useful_snippets.py
@@ -1,1 +1,5 @@
+# Drop duplicates based on a column
 df.drop_duplicates(subset=['column_foo'], inplace=True)
+
+# Drop rows based on some condition
+df.drop(df['column_foo'].isnull().index, inplace=True)
