c49078558f74bbb6dd66db45e7a6ffd6ab294851
Nikhil Raju <nikhilraju.1511@gmail.com>
2021-07-08 14:32:11 -0400

    print a row with many columns for debugging

--- pandas_useful_snippets.py
+++ pandas_useful_snippets.py
@@ -3,3 +3,7 @@ df.drop_duplicates(subset=['column_foo'], inplace=True)
 
 # Drop rows based on some condition
 df.drop(df[df['column_foo'].isnull() ==True].index, inplace=True)
+
+
+# To print a single row values for debugging when there is a large number of columns (e.g 100 columns) 
+df.iloc[0].T.values
